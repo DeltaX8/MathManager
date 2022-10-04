b0ed0d4f9652d22130e17fa315e487a9c777b6f7
schizo5303 <114773667+schizo5303@users.noreply.github.com>
2022-10-03 21:36:45 -0500

    Update login.js

--- data/login.js
+++ data/login.js
@@ -1,1 +1,15 @@
+// Made by Schizo5303
 
+// VARIABLES
+
+var pass = document.getElementById("userPass");
+var pwrd = "12822";
+
+
+function logIn()
+{
+  if (pass == pwrd)
+  {
+    window.open("data/main.html"); 
+  }
+}
